dc8811e49fff52af7f620ff485526dac37165236
Andrius Rimkus <139064236+Govdas@users.noreply.github.com>
2023-10-17 16:17:59 +0100

    Basic methods, variables, expression, concat

---
+++ Basic-JavaScript-Projects/JS/main.js
@@ -0,0 +1,12 @@
+//window.alert() method
+window.alert("Hello, World!");
+
+//Assigning variables to values
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string"
+
+//Concatenating the Sent1 and Sent2 variables together
+document.write(Sent1 + Sent2);
+
+//Statement containing an expression (5 + 5)
+document.write(5 + 5);
